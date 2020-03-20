d95b1702918b84c8a7acc44bcd8ecc2aa18ec761
Yu-hanCheng <sarahcheng1231@gmail.com>
2020-03-20 11:33:25 +0800

    feat: auth middleware

--- routes/web.php
+++ routes/web.php
@@ -13,14 +13,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () { return view('login');})->name('login.view');
 Route::get('register', function () { return view('register');})->name('register.view');
+Route::get('/', function () { return view('login');})->name('login.view');
 Route::post('login','UserController@login')->name('login');
-Route::get('logout','UserController@logout')->name('logout');
-Route::post('register','UserController@store')->name('register');
-Route::post('post','PostController@store')->name('post');
-Route::get('board','PostController@index')->name('board.show');
-Route::post('comment','CommentController@store')->name('comment');
-Route::get('comment','CommentController@index')->name('comment.show');
-Route::post('reply','ReplyController@store')->name('reply');
-Route::post('like','LikeController@store')->name('like');
+
+Route::group(['middleware' => ['diyauth']], function(){
+    Route::get('logout','UserController@logout')->name('logout');
+    Route::post('register','UserController@store')->name('register');
+    Route::post('post','PostController@store')->name('post');
+    Route::get('board','PostController@index')->name('board.show');
+    Route::post('comment','CommentController@store')->name('comment');
+    Route::get('comment','CommentController@index')->name('comment.show');
+    Route::post('reply','ReplyController@store')->name('reply');
+    Route::post('like','LikeController@store')->name('like');
+});
