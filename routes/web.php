5154825ea9b9c9e77670ab80f1a1f1680ec38013
Yu-hanCheng <sarahcheng1231@gmail.com>
2020-03-25 17:00:40 +0800

    [REFACTOR] Del web/views

--- routes/web.php
+++ routes/web.php
@@ -13,17 +13,3 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('register', function () { return view('register');})->name('register.view');
-Route::get('/', function () { return view('login');})->name('login.view');
-Route::post('login','UserController@login')->name('login');
-Route::post('register','UserController@store')->name('register');
-
-Route::group(['middleware' => ['diyauth']], function(){
-    Route::get('logout','UserController@logout')->name('logout');
-    Route::post('post','PostController@store')->name('post');
-    Route::get('board','PostController@index')->name('board.show');
-    Route::post('comment','CommentController@store')->name('comment');
-    Route::get('comment','CommentController@index')->name('comment.show');
-    Route::post('reply','ReplyController@store')->name('reply');
-    Route::post('like','LikeController@store')->name('like');
-});
