0bf6cbeacf15cf3dcfc49280de94c23aa21f9bc4
Yu-hanCheng <sarahcheng1231@gmail.com>
2020-03-27 07:59:31 +0800

    [FIX] add Already liked

--- app/Http/Controllers/LikeController.php
+++ app/Http/Controllers/LikeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Like;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Validator;
 
 class LikeController extends Controller
 {
@@ -14,6 +15,18 @@ class LikeController extends Controller
 
     public function store(Request $request, $id)
     {
+        $va = Validator::make( ['post_id'=>$id], [
+            'post_id' => 'required|exists:posts,id'
+        ]);
+        if ($va->fails()) {
+            return response()->json(['msg'=>$va->errors()],416);
+        }
+        if (Like::where([
+            ['post_id','=',$id],
+            ['user_id','=',$request->user()->id],
+            ])->get()->count() > 0) {
+            return response()->json(["msg" => "Already liked!"], 400);
+        }
         $array = [
             'post_id' => $id,
             'user_id' => $request->user()->id,
