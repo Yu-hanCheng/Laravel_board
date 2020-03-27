7bb1f7f223da52f622b94d07f9129e216e227c17
Yu-hanCheng <sarahcheng1231@gmail.com>
2020-03-27 16:16:17 +0800

    [STYLE] delete useless code

--- app/Post.php
+++ app/Post.php
@@ -7,13 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     protected $guarded = [];
-
     protected $hidden = [
         'parent_id','updated_at','layer','user_id'
     ];
 
     public function replies () {
-        return $this->hasMany(self::class,'parent_id')->where('layer',2);
+        return $this->hasMany(self::class,'parent_id');
     }
 
     public function comments() {
@@ -31,16 +30,4 @@ class Post extends Model
     public function user () {
         return $this->belongsTo(User::class);
     } 
-
-    public static function showComments($post_id)
-    {
-        $allcommentsWithReplies = Post::with(['replies' => function ($query){
-            $query->with('user')->orderBy('created_at','desc')->get();
-        },'user'])->where('parent_id',$post_id)->orderBy('created_at','desc')->get();
-        $post = Post::with('likes')->find($post_id);
-        $response['post'] = $post->only('id','content');
-        $response['likes'] = $post->likes;
-        $response['Allcomments'] = $allcommentsWithReplies;
-        return $response;
-    }
 }
