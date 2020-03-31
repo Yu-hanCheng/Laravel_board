fa648faf748aa5080bb64c7c9a57b1e039bfe36c
Yu-hanCheng <sarahcheng1231@gmail.com>
2020-03-31 09:42:58 +0800

    [REFACTOR] del layer on Posts/ cleanify relation function in post model

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,26 +8,16 @@ use App\Post;
 
 class PostController extends Controller
 {
-    const LAYER_POST = 0;
-
     public function index(Request $request)
     {
-        $all = Post::with([
-            'comments' => function ($query) {
-                    $query->with([
-                        'replies' => function ($query) {
-                            $query->with('user')->orderBy('created_at', 'desc')->get();
-                        },
-                        'user']
-                        )->orderBy('created_at', 'desc')->get();
-                    },
-            'likeList',
-            'user'])
-            ->withCount(['isLike' => function ($query) use ($request) {
-                    $query->where('user_id', $request->user()->id ?? 0);
-                    }])
-            ->where('layer', self::LAYER_POST) 
-            ->orderBy('created_at', 'desc')->get();
+        $all = Post::where('parent_id', null)
+            ->with([
+                'comments', 
+                'likeList',
+                'user'])
+            ->withCount('isLike')
+            ->orderBy('updated_at', 'desc')
+            ->get();
         return response()->json(["posts" => $all], 200);
     }
 
@@ -35,7 +25,6 @@ class PostController extends Controller
     {
         $va = Validator::make($request->all(), [
             'parent_id' => 'nullable|integer|min:1',
-            'layer' =>'required|integer|between:0,2',
             'content' => 'required|max:205',
         ]);
         
@@ -43,18 +32,11 @@ class PostController extends Controller
             return response()->json(['msg' => (string)$va->errors()], 416);
         }
         
-        if ($request['parent_id'] == ""  and  $request['layer'] != self::LAYER_POST){
-            return response()->json(["msg" => "invalid layer"], 400); 
-        } elseif ($request['parent_id'] != ""  and  $request['layer'] == self::LAYER_POST) {
-            return response()->json(["msg" => "invalid layer"], 400); 
-        } else {
-            Post::create([
-                'user_id' => $request->user()->id,
-                'parent_id' => $request['parent_id'],
-                'layer' => $request['layer'],
-                'content' => $request['content'],
-            ]);
-            return response()->json(["msg" => "successfully"], 201);
-        }
+        $post = Post::create([
+            'user_id' => $request->user()->id,
+            'parent_id' => $request['parent_id'],
+            'content' => $request['content'],
+        ]);
+        return response()->json(["msg" => $post], 201);
     }
 }
--- app/Post.php
+++ app/Post.php
@@ -8,33 +8,39 @@ class Post extends Model
 {
     protected $guarded = [];
     protected $hidden = [
-        'parent_id', 'updated_at', 'layer', 'user_id'
+        'parent_id', 'created_at', 'user_id'
     ];
     protected $casts = [
         'created_at' => 'timestamp',
         'updated_at' => 'timestamp',
     ];
-
+   
     public function replies () 
     {
-        return $this->hasMany(self::class, 'parent_id');
+        return $this->hasMany(self::class, 'parent_id')
+                    ->with('user')
+                    ->orderBy('updated_at', 'desc');
     }
 
     public function comments() 
     {
-        return $this->hasMany(self::class, 'parent_id');
+        return $this->hasMany(self::class, 'parent_id')
+                    ->with('replies','user')
+                    ->orderBy('updated_at', 'desc');
     }
 
     public function likeList () 
     {
-        return $this->hasMany(Like::class, 'post_id')->with('user');
-    }
-    
-    public function isLike () 
-    {
-        return $this->hasMany(Like::class, 'post_id');
+        return $this->hasMany(Like::class, 'post_id')
+                    ->with('user');
     }
 
+    public function isLike() 
+    {
+        return $this->hasMany(Like::class, 'post_id')
+                    ->where('user_id', auth()->check() ? auth()->user()->id : 0);
+    }
+    
     public function user () 
     {
         return $this->belongsTo(User::class);
